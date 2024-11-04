66ccb60cf35d7ad3e15ae81be172fa4da210ac0e
xtyxe <142973923+pdiana6@users.noreply.github.com>
2024-11-04 17:47:56 +0500

    project

---
+++ Assets/Scripts&Cats/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Эта штука запускается один раз
+    void Start()
+    {
+        print("Hello, world!");
+    }
+
+    // А она обновляется каждый кадр
+    void Update()
+    {
+        
+    }
+}
