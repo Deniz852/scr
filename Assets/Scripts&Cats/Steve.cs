5de0dabbaf807a1531ba20bc950d430057ccc6e0
Deniz852 <denisdvcube@gmail.com>
2024-11-04 18:15:34 +0500

    /

--- Assets/Scripts&Cats/Steve.cs
+++ Assets/Scripts&Cats/Steve.cs
@@ -7,13 +7,17 @@ public class Steve : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =new Vector3(0, 0, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =new Vector3(-0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =new Vector3(0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =new Vector3(-0.5f, 2, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =new Vector3(0, 3, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+
+    }
+
     // Update is called once per frame
-        void Update()
+    void Update()
     {
         
     }
